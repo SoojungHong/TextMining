4af05649f50646e74d70967d2f0358ccfd8c54f0
Soojung <hong.soojung@gmail.com>
2018-06-24 15:05:54 +0200

    init for advanced text processing

---
+++ AdvancedTextProcessing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 14:58:59 2018
+
+@author: soojunghong
+
+@About : advanced Text processing to extract features using NLP Techniques 
+"""
+
+# N-grams 
+
