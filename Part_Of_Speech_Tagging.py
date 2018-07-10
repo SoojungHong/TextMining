dc33d07bfd8e9ab6d391adc2b8bb704003e124e7
Soojung <hong.soojung@gmail.com>
2018-07-10 22:56:56 +0200

    Part of Speech tagging

--- Part_Of_Speech_Tagging.py
+++ Part_Of_Speech_Tagging.py
@@ -41,8 +41,8 @@ def features(sentence, index):
             'prefix-1': sentence[index][0],
             'prefix-2': sentence[index][:2],
             'prefix-3': sentence[index][:3],
-            'suffix-1': sentence[index][-1], # ??
-            'suffix-2': sentence[index][-2:], ## ??
+            'suffix-1': sentence[index][-1], # -1 means the index from the very right end
+            'suffix-2': sentence[index][-2:], ## second right end 
             'suffix-3': sentence[index][-3:],
             'prev_word': '' if index == 0 else sentence[index-1],
             'next_word': '' if index == len(sentence)-1 else sentence[index+1],
@@ -53,3 +53,57 @@ def features(sentence, index):
 
 import pprint
 pprint.pprint(features(['This', 'is', 'a', 'sentence'], 2))    
+
+
+def untag(tagged_sentence):
+    return [w for w, t in tagged_sentence]
+
+# build training set
+# Split the dataset for training and testing 
+cutoff = int(.75 * len(tagged_sentences))
+training_sentences = tagged_sentences[:cutoff]
+training_sentences
+test_sentences = tagged_sentences[cutoff:]
+test_sentences 
+
+print len(training_sentences)
+print len(test_sentences)
+
+def transform_to_dataset(tagged_sentences):
+    X, y = [],[]
+    
+    for tagged in tagged_sentences:
+        for index in range(len(tagged)):
+            X.append(features(untag(tagged), index))
+            y.append(tagged[index][1])
+    return X, y
+
+X, y = transform_to_dataset(training_sentences)        
+X
+y
+
+# train the classifier - Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction import DictVectorizer 
+from sklearn.pipeline import Pipeline
+
+clf = Pipeline([
+        ('vectorizer', DictVectorizer(sparse=False)),
+        ('classifier', DecisionTreeClassifier(criterion='entropy'))
+        ])
+    
+clf.fit(X[:10000], y[:10000]) #use first 10k samples
+
+print 'Training completed'
+
+X_test, y_test = transform_to_dataset(test_sentences)
+
+print "Accuracy: ", clf.score(X_test, y_test)
+
+# let's use classifier
+def pos_tag(sentence):
+    tagged_sentence = []
+    tags = clf.predict([features(sentence, index) for index in range(len(sentence))])  
+    return zip(sentence, tags)
+        
+print pos_tag(word_tokenize('this is my friend John'))
