8739e4e4f90b18bbd59d007de5c1c42a33fd55b9
Soojung Hong <hong.soojung@gmail.com>
2019-03-08 18:24:59 +0100

    Create Gaussian_Mixture_Model_on_text_data_clustering.py

---
+++ Gaussian_Mixture_Model_on_text_data_clustering.py
@@ -0,0 +1,373 @@
+# libraries
+
+import collections
+import re
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from experiments.place_category import joined_with_place_info
+from experiments.clustering import *
+from lib.data_utils import *
+from lib.file_utils import *
+from lib.geo_utils import *
+from lib.visual_utils import *
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def load_google_w2v_model():
+    from gensim.models import KeyedVectors
+    filename = 'C:/Users/shong/Downloads/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin'
+    # binary file 3.4GB
+    model = KeyedVectors.load_word2vec_format(filename, binary=True)  # 43 seconds to load
+    return model
+
+
+def get_all_queries_contains(query, df):
+    contain_res_df = df[df['query_raw'].str.contains(query)]
+    queries_contains_restaurant = contain_res_df['query_raw']
+    return queries_contains_restaurant.unique()
+
+
+def get_long_queries_contains(query, df):
+    all_queries = df['query_raw'].unique()
+    for i in range(len(all_queries)):
+        if all_queries[i] == query:
+            return query
+
+
+def get_one_token_query_set(queries):
+    one_token_set = list()
+    for i in range(len(queries)):
+        current_query = queries[i]  # narray
+
+        if len(current_query.split()) is 1:
+            # remove special character # ToDo : handle the special char but dataset contains special char at the moment
+            # cleanStr = re.sub('\W+','', current_query)
+            # print(cleanStr)
+            one_token_set.append(current_query)
+
+    return one_token_set
+
+
+def get_word_mover_distance(w2v_model, doc1, doc2):
+    dist = w2v_model.wmdistance(doc1, doc2)
+    return dist
+
+
+def get_category_names(pid):
+    match = merged_data[merged_data['ppid'] == pid]
+    if match.empty:
+        return None
+    else:
+        return match['category_names'].iloc[0]  # list
+
+
+def get_word_vector(model, word):
+    print(word)
+    word = re.sub('\W+', ' ', word)
+    tokens = word.split()
+    word_vec = ''
+    if len(tokens) > 1:
+        for w in range(len(tokens)):
+            cleaned_word = re.sub('\W+', '', tokens[w])
+            curr_vec = model.get_vector(cleaned_word)
+            print(curr_vec)
+    else:
+        word = re.sub('\W+', '', word)
+        word = word.lower()
+        if word in model.vocab:
+            word_vec = model.get_vector(word)
+            print(word_vec)
+    return word_vec
+
+
+def get_food_type(pid):
+    match = merged_data[merged_data['ppid'] == pid]
+    if match.empty:
+        print("empty")
+        return None
+    else:
+        return match['food_type'].iloc[0]  # list
+
+
+def get_closest_category(category_list, model, pname):
+    min_dist = sys.maxsize
+    closest_cat_name = ""
+    if type(category_list) is type(None):
+        print('NoneType')
+    else:
+        if len(category_list) > 0:
+            for c in range(len(category_list)):
+                curr_category = re.sub('\W+', ' ',  category_list[c])
+                place = re.sub('\W+', '', pname)
+                distance = model.wmdistance(pname, curr_category)
+                if distance < min_dist:
+                    min_dist = distance
+                    closest_cat_name = category_list[c]
+            #print(min_dist)
+            #print(closest_cat_name)
+            #get_word_vector(model, closest_cat_name)
+    return (re.sub('\W+', ' ', closest_cat_name)).lower()
+
+
+def get_closest_foodtype(food_type_list, model, pname):
+    # ToDo : do check if the food_type set is empty - if it is empty, return empty, empty food type will be added as a adjective
+    min_dist = sys.maxsize
+    closest_food_name = ""
+    if type(food_type_list) is type(None):
+        print('NoneType')
+    else:
+        if len(food_type_list) > 0:
+            for c in range(len(food_type_list)):
+                curr_food_type = re.sub('\W+', '', food_type_list[c])
+                place = re.sub('\W+', '', pname)
+                distance = model.wmdistance(place, curr_food_type)
+                #print(curr_food_type, distance)
+                if distance < min_dist:
+                    min_dist = distance
+                    closest_food_name = food_type_list[c]
+            #print(min_dist)
+            #print(closest_food_name
+            #get_word_vector(model, closest_food_name)
+    return (re.sub('\W+', ' ', closest_food_name)).lower()
+
+
+def compose_place_description_by_word2vec(category_name, food_type):
+    place_desc = food_type + "" + category_name
+    return place_desc
+
+
+def represent_to_vector(model, desc):
+    split_desc = desc.split()
+    # remove special charactor
+    #sum_all_vec = np.ndarray((300,), float)  # error - the vector is initialized with random values
+    sum_all_vec = np.zeros((300,), dtype=float)
+
+    for des_i in range(len(split_desc)):
+        token = re.sub('\W+', '', split_desc[des_i])
+        if token in model.vocab:
+            desc_vec = model.get_vector(token)
+            sum_all_vec = sum_all_vec + desc_vec
+
+    return sum_all_vec
+
+
+def pca_place_description(df):
+    # visualize the word vector (description to one vector)
+    import numpy as np
+    from sklearn.decomposition import PCA
+    from matplotlib import pyplot
+    pca = PCA(n_components=2)  # number of components to keep
+    result = pca.fit_transform((df['desc_vec'].values).tolist())
+    # create a scatter plot of the projection
+    pyplot.scatter(result[:, 0], result[:, 1])
+    places = list(final_df['place_desc'].values)
+    for i, word in enumerate(places):
+        pyplot.annotate(word, xy=(result[i, 0], result[i, 1]), fontsize=8)
+    pyplot.show()
+
+
+def get_vec_by_pre_trained_doc2vec(doc2vec_model, input_str):
+    tokens = input_str.split()
+    doc_vec = doc2vec_model.infer_vector(tokens)
+    return doc_vec
+
+
+def get_desc_vec_by_pre_trained_doc2vec(model, input_str):
+    tokens = input_str.split()
+    doc_vec = model.infer_vector(tokens)
+    return doc_vec
+
+
+def normalize(list, range): # range should be (lower_bound, upper_bound)
+    l = np.array(list)
+    a = np.max(l)
+    c = np.min(l)
+    b = range[1]
+    d = range[0]
+
+    m = (b - d) / (a - c)
+    pslope = (m * (l - c)) + d
+    return pslope
+
+
+def get_z_distance_normalized(x_data_list, y_data_list, z_data_list):
+    min_x = min(x_data_list)
+    max_x = max(x_data_list)
+    min_y = min(y_data_list)
+    max_y = max(y_data_list)
+    xy_ran = list()
+    xy_ran.append(min(min_x, min_y))
+    xy_ran.append(max(max_x, max_y))
+    z_normalized_list = normalize(z_data_list, xy_ran)
+    return z_normalized_list
+
+
+def reduce_dimension_PCA(df):
+    pca = PCA(n_components=4)  # number of components to keep
+    description_list = (df['desc_vec'].values).tolist()
+    query_vec = get_vec_by_pre_trained_doc2vec(m, df['query_raw'].iloc[0])
+    query_name = df['query_raw'].iloc[0]
+    description_list.append(query_vec)  # add the vector of the query itself as origin
+    result_df = pca.fit_transform(description_list)
+    return result_df
+
+
+def run_gaussian_mixture_model_and_plot(df):
+    gmm = GaussianMixture(n_components=2, n_init=10).fit(df)  # say, two clusters
+    ax = None  #ToDo : why I need this? Do clean up if it is not necessary
+    label = True
+    ax = ax or plt.gca()
+    labels = gmm.fit(df).predict(df)
+    colmap = {1: 'r', 2: 'g'}  # , 3:' b'}
+
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    colors = map(lambda x: colmap[x + 1], labels)
+
+    # plot the data point in the 3 dimensional space
+    ax.scatter(df['x'], df['y'], df['z'], c=labels, cmap='viridis', edgecolor='k', s=40, alpha=0.5)
+
+    # assign labels into points - place name, query(origin) name)
+    places = list(final_df['place_desc'].values)
+    result_index = list(final_df['result_index'].values)
+    place_names = list(final_df['name_chosen'].values)
+    origin_name = final_df['query_raw'].iloc[0]
+
+    for i in range(len(result)):
+        if i == (len(result) - 1):
+            point_labels.append(origin_name)
+        else:
+            point_labels.append(place_names[i] + ':' + places[i] + ':' + str(result_index[i]))
+
+    # add the text label for each data points next to data point
+    for x_label, y_label, z_label, label in zip(df['x'], df['y'], df['z'], point_labels):
+        ax.text(x_label, y_label, z_label, label, size=6)
+
+    pyplot.show()
+
+
+def run_kmeans_clustering(df, final_df):
+    from sklearn.cluster import KMeans
+    kmeans = KMeans(n_clusters=2)
+    kmeans.fit(df)
+
+    labels = kmeans.predict(df)
+    centroids = kmeans.cluster_centers_
+
+    from matplotlib import pyplot
+    from mpl_toolkits.mplot3d import Axes3D
+    colmap = {1: 'r', 2: 'g'}  # , 3:' b'}
+
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    colors = map(lambda x: colmap[x + 1], labels)
+
+    ax.scatter(df['x'], df['y'], df['z'], c=labels, cmap='viridis', edgecolor='k', s=40, alpha=0.5)
+
+    # give the labels to each point
+    places = list(final_df['place_desc'].values)
+    result_index = list(final_df['result_index'].values)
+    place_names = list(final_df['name_chosen'].values)
+    origin_name = final_df['query_raw'].iloc[0]
+    for i in range(len(result)):
+        if i == (len(result) - 1):
+            point_labels.append(origin_name)
+        else:
+            point_labels.append(place_names[i] + ':' + str(result_index[i]))  # + ':' + places[i])
+
+    for x_label, y_label, z_label, label in zip(df['x'], df['y'], df['z'], point_labels):
+        ax.text(x_label + 0.02, y_label + 0.02, z_label + 0.01, label, size=6)
+
+    for idx, centroid in enumerate(centroids):
+        ax.scatter(*centroid, cmap=colmap[idx + 1])
+        ax.text(centroid[0], centroid[1], centroid[2] + 0.1, 'cluster_{}'.format(idx), None, size=6, color='Red')
+
+    pyplot.show()
+
+
+# data
+RECO_HEADER = [#somedata]
+RECO_DATA = "C:hahaha.csv"
+
+# read data
+df = read_file(RECO_DATA, RECO_HEADER)  # (209921, 18)
+
+# get records contain given the input query
+ret_query = get_long_queries_contains("'mexican restaurants near my location'", df)  # "'african restaurant near me'", df)
+print('given query :', ret_query)
+
+# get all recommended place records on query and same query location
+all_records = df[df['query_raw'] == ret_query]
+all_timestamp = all_records['timestamp'].unique()
+
+one_query_records = all_records[all_records['timestamp'] == all_timestamp[0]]  # we check only one timestamp which execute one query
+print('query result shape :', one_query_records.shape)
+
+one_query_records['distance'] = one_query_records.apply(lambda row: measure_distance(row.query_lat, row.query_lon, row.result_lat, row.result_lon), axis=1)
+
+# get joined place data frame
+merged_data = joined_with_place_info()
+
+# add columns 'category_names' and 'food_type'
+one_query_records['category_names'] = one_query_records.apply(lambda row: get_category_names(row.result_name), axis=1)
+one_query_records['food_type'] = one_query_records.apply(lambda row: get_food_type(row.result_name), axis=1)
+
+# for places are not related with restaurant such as gas station
+#one_query_records.to_csv('C:/Users/shong/Documents/data/one_query_records.csv')
+
+# google word2vec
+model = load_google_w2v_model()
+one_query_records['closest_category'] = one_query_records.apply(lambda row: get_closest_category(row.category_names, model, row.name_chosen), axis=1)
+one_query_records['closest_food_type'] = one_query_records.apply(lambda row: get_closest_foodtype(row.food_type, model, row.name_chosen), axis=1)
+one_query_records['place_desc'] = one_query_records.apply(lambda row: compose_place_description_by_word2vec(row.closest_category, row.closest_food_type), axis=1)
+
+# Prepare data frame which does not contain empty category or empty food type
+final_df = one_query_records[one_query_records['place_desc'] != '']
+
+# option1 using word2vec
+# final_df['desc_vec'] = final_df.apply(lambda row: represent_to_vector(model, row.place_desc), axis=1)
+
+# option2 using doc2vec
+import gensim.models as g
+
+# load model
+pre_trained_model = 'C:/Users/shong/Downloads/enwiki_dbow/enwiki_dbow/doc2vec.bin'
+m = g.Doc2Vec.load(pre_trained_model)
+
+# add 'desc_vec' : vector of place description
+final_df['desc_vec'] = final_df.apply(lambda row: get_vec_by_pre_trained_doc2vec(m, row.place_desc), axis=1)
+
+# recommendation data frame
+labels = ['query_raw', 'name_chosen', 'place_desc', 'desc_vec', 'distance']
+result = reduce_dimension_PCA(final_df)
+
+# write the final experiment dataframe to csv file
+#final_df.to_csv('C:/Users/shong/Documents/data/final_df_mexican_near_restaurant.csv')
+
+# prepare z-axis value (distance value), normalized z-value
+z_distance = (final_df['distance'].values).tolist()
+z_distance.append(0)
+x_data = result[:, 0]
+y_data = result[:, 1]
+point_labels = []
+
+# find range of x and y value, normalize the distance (z values)
+z_normalized = get_z_distance_normalized(x_data, y_data, z_distance)
+
+df = pd.DataFrame({
+    'x': x_data,
+    'y': y_data,
+    'z': z_normalized
+})
+
+# clustering with Gaussian Mixture Model
+run_gaussian_mixture_model_and_plot(df)
+
+# clustering with K-means algorithm
+run_kmeans_clustering(df, final_df)
